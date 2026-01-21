9320323b4230724ebf979e092c7d6cdf49f4d33b
cloudinmotion-acc <aaleti.bharath@accenture.com>
2026-01-21 13:10:45 +0530

    updated image

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,7 @@
 variable "image" {
   description = "Docker image to use for the application"
   type        = string
+  default     = "310378384655.dkr.ecr.us-east-1.amazonaws.com/llm-module:v1"
 }
 
 variable "openai_api_key" {
